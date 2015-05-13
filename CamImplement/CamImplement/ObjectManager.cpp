c5627055f70d4b11650af2f6c7cc091238c42201
Anton Åkesson <mopaqrr@hotmail.com>
2015-05-13 13:43:15 +0200

    Performance upgrade!
    Reduced direct3d api calls.
    Much better performance.

--- CamImplement/CamImplement/ObjectManager.cpp
+++ CamImplement/CamImplement/ObjectManager.cpp
@@ -1,5 +1,8 @@
 #include "ObjectManager.h"
 
+using namespace DirectX;
+using namespace std;
+
 ObjectManager::ObjectManager()
 {
 	m_loader = nullptr;
@@ -17,88 +20,96 @@ ObjectManager::ObjectManager(const ObjectManager& obj)
 
 ObjectManager::~ObjectManager()
 {
-
+	if (m_objPlayer)
+		delete m_objPlayer;
+	if (m_objEnemies)
+		delete m_objEnemies;
+	if (m_objTiles)
+		delete m_objTiles;
+	if (m_objObstacles)
+		delete m_objObstacles;
 }
 
-void ObjectManager::InitInstances(Object obj, ObjectInstance** arr, int size)
+void ObjectManager::InitInstances(Object obj, ObjectInstance *&object)
 {
-	if (size <= 0) { return; }
-
-	*arr = new ObjectInstance[size];
+	// We only need 1 instance of each object.
+	// Apply buffer -> draw multiple times using differente cBuffers.
 
 	float x, y, z;
 	float u, v;
 	float nx, ny, nz;
 
+	// [todo. Error check getObject]
 	ObjectType temp = m_loader->getObject(obj);
-	for (int i = 0; i < size; i++)
+	// [if (!temp)
+	// return;
+
+	object = new ObjectInstance();
+
+	object->nVertices = m_loader->getVertexCount(obj);
+	object->nNormals = m_loader->getVertexCount(obj);
+	object->nIndices = m_loader->getIndexCount(obj);
+
+	object->input = new InputType[object->nVertices];
+	//object->.normals = new NormalType[(*arr)[i].nNormals];
+	object->indices = new UINT[object->nIndices];
+
+	//-- Used for testing, to be removed -------------------------
+	switch (obj)
 	{
-		(*arr)[i].nVertices = m_loader->getVertexCount(obj);
-		(*arr)[i].nNormals = m_loader->getVertexCount(obj);
-		(*arr)[i].nIndices = m_loader->getIndexCount(obj);
-
-		(*arr)[i].input = new InputType[(*arr)[i].nVertices];
-		//(*arr)[i].normals = new NormalType[(*arr)[i].nNormals];
-		(*arr)[i].indices = new UINT[(*arr)[i].nIndices];
-
-		//-- Used for testing, to be removed -------------------------
-		switch (obj)
+		case Player:
 		{
-			case Player:
-			{
-				u = 1.0f;
-				v = 1.0f;
-				break;
-			}
-			case Enemy:
-			{
-				u = 1.0f;
-				v = 0.0f;
-				break;
-			}
-			case Tile:
-			{
-				u = 0.0f;
-				v = 1.0f;
-				break;
-			}
-			default:
-			{
-				u = 0.0f;
-				v = 0.0f;
-				break;
-			}
+			u = 1.0f;
+			v = 1.0f;
+			break;
 		}
-		//------------------------------------------------------------
-
-		for (int j = 0; j < (*arr)[i].nVertices; j++)
+		case Enemy:
 		{
-			x = temp.vertices[j].x;
-			y = temp.vertices[j].y;
-			z = temp.vertices[j].z;
-			//u = temp.texCoords[j].u;
-			//v = temp.texCoords[j].v;
-			nx = temp.normals[j].x;
-			ny = temp.normals[j].y;
-			nz = temp.normals[j].z;
-
-			(*arr)[i].input[j] = InputType(x, y, z, u, v, nx, ny, nz);
+			u = 1.0f;
+			v = 0.0f;
+			break;
 		}
-
-		int k = 0;
-		for (int j = 0; j < (*arr)[i].nIndices / 3; j++)
+		case Tile:
 		{
-			(*arr)[i].indices[k] = temp.faces[j].vIndex1 - 1;
-			(*arr)[i].indices[k + 1] = temp.faces[j].vIndex2 - 1;
-			(*arr)[i].indices[k + 2] = temp.faces[j].vIndex3 - 1;
-			k += 3;
+			u = 0.0f;
+			v = 1.0f;
+			break;
 		}
-
-		DirectX::XMStoreFloat4x4(&(*arr)[i].world, DirectX::XMMatrixIdentity());
-		(*arr)[i].vertexBuffer = nullptr;
-		(*arr)[i].indexBuffer = nullptr;
-		(*arr)[i].texture = nullptr;
+		default:
+		{
+			u = 0.0f;
+			v = 0.0f;
+			break;
+		}
 	}
+	//------------------------------------------------------------
+
+	for (int j = 0; j < object->nVertices; j++)
+	{
+		x = temp.vertices[j].x;
+		y = temp.vertices[j].y;
+		z = temp.vertices[j].z;
+		//u = temp.texCoords[j].u;
+		//v = temp.texCoords[j].v;
+		nx = temp.normals[j].x;
+		ny = temp.normals[j].y;
+		nz = temp.normals[j].z;
+
+		object->input[j] = InputType(x, y, z, u, v, nx, ny, nz);
+	}
+
+	int k = 0;
+	for (int j = 0; j < object->nIndices / 3; j++)
+	{
+		object->indices[k] = temp.faces[j].vIndex1 - 1;
+		object->indices[k + 1] = temp.faces[j].vIndex2 - 1;
+		object->indices[k + 2] = temp.faces[j].vIndex3 - 1;
+		k += 3;
+	}
+
+	object->vertexBuffer = nullptr;
+	object->indexBuffer = nullptr;
+	object->texture = nullptr;
 }
 
 void ObjectManager::CreateBuffers(ID3D11Device* device)
@@ -116,69 +127,74 @@ void ObjectManager::CreateBuffers(ID3D11Device* device)
 
 	//Vertex buffer
 	D3D11_BUFFER_DESC vertexBufferDesc;
-	vertexBufferDesc.Usage = D3D11_USAGE_DYNAMIC;
-	vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objPlayer->nVertices;
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = 0;
 	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-	vertexBufferDesc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	vertexBufferDesc.CPUAccessFlags = 0;
 	vertexBufferDesc.MiscFlags = 0;
 	vertexBufferDesc.StructureByteStride = 0;
 
-	int size = sizeof(*m_objPlayer->input);
-
 	D3D11_SUBRESOURCE_DATA vData;
-	vData.pSysMem = m_objPlayer->input;
+	vData.pSysMem = nullptr;
 	vData.SysMemPitch = 0;
 	vData.SysMemSlicePitch = 0;
 
 	//Index buffer
 	D3D11_BUFFER_DESC indexBufferDesc;
 	indexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
-	indexBufferDesc.ByteWidth = sizeof(UINT) * m_objPlayer->nIndices;
+	indexBufferDesc.ByteWidth = 0;
 	indexBufferDesc.BindFlags = D3D11_BIND_INDEX_BUFFER;
 	indexBufferDesc.CPUAccessFlags = 0;
 	indexBufferDesc.MiscFlags = 0;
 	indexBufferDesc.StructureByteStride = 0;
 
 	D3D11_SUBRESOURCE_DATA iData;
-	iData.pSysMem = m_objPlayer->indices;
+	iData.pSysMem = nullptr;
 	iData.SysMemPitch = 0;
 	iData.SysMemSlicePitch = 0;
 
 	//Player buffers
-	device->CreateBuffer(&vertexBufferDesc, &vData, &m_objPlayer->vertexBuffer);
-	device->CreateBuffer(&indexBufferDesc, &iData, &m_objPlayer->indexBuffer);
-
-	//Enemy buffers
-	for (int i = 0; i < m_nEnemies; i++)
+	if (m_objPlayer)
 	{
-		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objEnemies[i].nVertices;
-		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objEnemies[i].nIndices;
-		vData.pSysMem = m_objEnemies[i].input;
-		iData.pSysMem = m_objEnemies[i].indices;
-		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objEnemies[i].vertexBuffer);
-		device->CreateBuffer(&indexBufferDesc, &iData, &m_objEnemies[i].indexBuffer);
+		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objPlayer->nVertices;
+		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objPlayer->nIndices;
+		vData.pSysMem = m_objPlayer->input;
+		iData.pSysMem = m_objPlayer->indices;
+		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objPlayer->vertexBuffer);
+		device->CreateBuffer(&indexBufferDesc, &iData, &m_objPlayer->indexBuffer);
+	}
+
+	//Enemy buffer
+	if (m_objEnemies)
+	{
+		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objEnemies->nVertices;
+		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objEnemies->nIndices;
+		vData.pSysMem = m_objEnemies->input;
+		iData.pSysMem = m_objEnemies->indices;
+		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objEnemies->vertexBuffer);
+		device->CreateBuffer(&indexBufferDesc, &iData, &m_objEnemies->indexBuffer);
 	}
 
 	//Obstacle buffers
-	for (int i = 0; i < m_nObstacles; i++)
+	if (m_objObstacles)
 	{
-		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objObstacles[i].nVertices;
-		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objObstacles[i].nIndices;
-		vData.pSysMem = m_objObstacles[i].input;
-		iData.pSysMem = m_objObstacles[i].indices;
-		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objObstacles[i].vertexBuffer);
-		device->CreateBuffer(&indexBufferDesc, &iData, &m_objObstacles[i].indexBuffer);
+		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objObstacles->nVertices;
+		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objObstacles->nIndices;
+		vData.pSysMem = m_objObstacles->input;
+		iData.pSysMem = m_objObstacles->indices;
+		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objObstacles->vertexBuffer);
+		device->CreateBuffer(&indexBufferDesc, &iData, &m_objObstacles->indexBuffer);
 	}
 
 	//Tile buffers
-	for (int i = 0; i < m_nTiles; i++)
+	if (m_objTiles)
 	{
-		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objTiles[i].nVertices;
-		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objTiles[i].nIndices;
-		vData.pSysMem = m_objTiles[i].input;
-		iData.pSysMem = m_objTiles[i].indices;
-		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objTiles[i].vertexBuffer);
-		device->CreateBuffer(&indexBufferDesc, &iData, &m_objTiles[i].indexBuffer);
+		vertexBufferDesc.ByteWidth = sizeof(InputType) * m_objTiles->nVertices;
+		indexBufferDesc.ByteWidth = sizeof(UINT) * m_objTiles->nIndices;
+		vData.pSysMem = m_objTiles->input;
+		iData.pSysMem = m_objTiles->indices;
+		device->CreateBuffer(&vertexBufferDesc, &vData, &m_objTiles->vertexBuffer);
+		device->CreateBuffer(&indexBufferDesc, &iData, &m_objTiles->indexBuffer);
 	}
 }
 
@@ -188,19 +204,28 @@ bool ObjectManager::LoadTextures(ID3D11Device* device)
 	return false;
 }
 
-void ObjectManager::RenderInstances(ID3D11DeviceContext* deviceContext, ObjectInstance* arr, int size)
+void ObjectManager::RenderInstances(ID3D11DeviceContext* deviceContext, ObjectInstance* arr)
 {
+	if (!arr)
+		return;
+
 	UINT stride = sizeof(InputType);
 	UINT offset = 0;
-	DirectX::XMMATRIX view = DirectX::XMLoadFloat4x4(&m_view);
-	DirectX::XMMATRIX projection = DirectX::XMLoadFloat4x4(&m_projection);
+	XMMATRIX view = XMLoadFloat4x4(&m_view);
+	XMMATRIX projection = XMLoadFloat4x4(&m_projection);
 
-	for (int i = 0; i < size; i++)
+	// Set buffers.
+	deviceContext->IASetIndexBuffer(arr->indexBuffer, DXGI_FORMAT_R32_UINT, offset);
+	deviceContext->IASetVertexBuffers(0, 1, &arr->vertexBuffer, &stride, &offset);
+
+	// Draw buffers for each world matrix.
+	for (UINT i = 0; i < arr->world.size(); i++)
 	{
-		DirectX::XMMATRIX world = DirectX::XMLoadFloat4x4(&arr[i].world);
-		DirectX::XMMATRIX wvp = world * view * projection;
-		DirectX::XMStoreFloat4x4(&cbPerObject.World, DirectX::XMMatrixTranspose(world));
-		DirectX::XMStoreFloat4x4(&cbPerObject.WVP, DirectX::XMMatrixTranspose(wvp));
+		// Update constant buffer.
+		XMMATRIX world = XMLoadFloat4x4(&arr->world[i]);
+		XMMATRIX wvp = world * view * projection;
+		XMStoreFloat4x4(&cbPerObject.World, XMMatrixTranspose(world));
+		XMStoreFloat4x4(&cbPerObject.WVP, XMMatrixTranspose(wvp));
 
 		D3D11_MAPPED_SUBRESOURCE cb;
 		ZeroMemory(&cb, sizeof(D3D11_MAPPED_SUBRESOURCE));
@@ -208,16 +233,10 @@ void ObjectManager::RenderInstances(ID3D11DeviceContext* deviceContext, ObjectIn
 		memcpy(cb.pData, &cbPerObject, sizeof(constBufferPerObject));
 		deviceContext->Unmap(cbPerObjectBuffer, 0);
 
-		//D3D11_MAPPED_SUBRESOURCE vb;
-		//ZeroMemory(&vb, sizeof(D3D11_MAPPED_SUBRESOURCE));
-		//deviceContext->Map(arr[i].vertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &vb);
-		//memcpy(vb.pData, arr[i].input, 32 * arr[i].nVertices);
-		//deviceContext->Unmap(arr[i].vertexBuffer, 0);
-
-		deviceContext->IASetIndexBuffer(arr[i].indexBuffer, DXGI_FORMAT_R32_UINT, offset);
-		deviceContext->IASetVertexBuffers(0, 1, &arr[i].vertexBuffer, &stride, &offset);
 		deviceContext->VSSetConstantBuffers(0, 1, &cbPerObjectBuffer);
-		deviceContext->DrawIndexed(arr[i].nIndices, 0, 0);
+
+		// Draw buffers.
+		deviceContext->DrawIndexed(arr->nIndices, 0, 0);
 	}
 }
 
@@ -227,73 +246,85 @@ void ObjectManager::Initialize(ID3D11Device* device, int nEnemies, int nObstacle
 	Object obj[] = { Player, Enemy, Tile };
 	m_loader->Initialize(obj, (sizeof(obj) / sizeof(Object)));
 
-	m_nEnemies = nEnemies;
-	m_nObstacles = nObstacles;
-	m_nTiles = nTiles;
-
-	InitInstances(Player, &m_objPlayer, 1);
-	InitInstances(Enemy, &m_objEnemies, m_nEnemies);
-	InitInstances(Obstacle, &m_objObstacles, m_nObstacles);
-	InitInstances(Tile, &m_objTiles, m_nTiles);
+	// Create meshes & buffers.
+	InitInstances(Player, m_objPlayer);
+	InitInstances(Enemy, m_objEnemies);
+	// InitInstances(Obstacle, m_objObstacles);
+	InitInstances(Tile, m_objTiles);
 
 	CreateBuffers(device);
 
-	DirectX::XMStoreFloat4x4(&cbPerObject.World, DirectX::XMMatrixIdentity());
-	DirectX::XMStoreFloat4x4(&cbPerObject.WVP, DirectX::XMMatrixIdentity());
+	// Create instances.
+	XMFLOAT4X4 mat;
+	XMStoreFloat4x4(&mat, XMMatrixIdentity());
+
+	m_objPlayer->world.push_back(mat);
+
+	for (INT i = 0; i < nEnemies; i++)
+		m_objEnemies->world.push_back(mat);
+	
+	for (INT i = 0; i < nObstacles; i++)
+		m_objObstacles->world.push_back(mat);
+
+	for (INT i = 0; i < nTiles; i++)
+		m_objTiles->world.push_back(mat);
+
+	XMStoreFloat4x4(&cbPerObject.World, XMMatrixIdentity());
+	XMStoreFloat4x4(&cbPerObject.WVP, XMMatrixIdentity());
 }
 
-void ObjectManager::SetPlayerWorld(const DirectX::XMMATRIX &world)
+void ObjectManager::SetPlayerWorld(const XMMATRIX &world)
 {
-	DirectX::XMStoreFloat4x4(&m_objPlayer->world, world);
+	XMStoreFloat4x4(&m_objPlayer->world[0], world);
 }
 
-void ObjectManager::SetEnemiesWorld(const DirectX::XMMATRIX* arr)
+void ObjectManager::SetEnemiesWorld(const XMMATRIX* arr)
 {
-	for (int i = 0; i < m_nEnemies; i++)
+	for (UINT i = 0; i < m_objEnemies->world.size(); i++)
 	{
 		SetEnemiesWorld(i, arr[i]);
 	}
 }
 
-void ObjectManager::SetEnemiesWorld(int index, const DirectX::XMMATRIX &world)
+void ObjectManager::SetEnemiesWorld(int index, const XMMATRIX &world)
 {
 	//Passing world directly into StoreFloat causes random access violation
-	DirectX::XMMATRIX w = world;
-	DirectX::XMStoreFloat4x4(&m_objEnemies[index].world, w);
+	XMMATRIX w = world;
+	XMStoreFloat4x4(&m_objEnemies->world[index], w);
 }
 
-void ObjectManager::SetObstaclesWorld(const DirectX::XMMATRIX* arr)
+void ObjectManager::SetObstaclesWorld(const XMMATRIX* arr)
 {
-	int size = sizeof(arr) / sizeof(DirectX::XMMATRIX);
-	if (size > m_nObstacles)
+	UINT size = sizeof(arr) / sizeof(XMMATRIX);
+	if (size > m_objObstacles->world.size())
 	{
-		size = m_nObstacles;
+		size = m_objObstacles->world.size();
 	}
 
-	for (int i = 0; i < size; i++)
+	for (UINT i = 0; i < m_objObstacles->world.size(); i++)
 	{
 		SetObstaclesWorld(i, arr[i]);
 	}
 }
 
-void ObjectManager::SetObstaclesWorld(int index, const DirectX::XMMATRIX &world)
+void ObjectManager::SetObstaclesWorld(int index, const XMMATRIX &world)
 {
-	DirectX::XMStoreFloat4x4(&m_objObstacles[index].world, world);
+	XMStoreFloat4x4(&m_objObstacles->world[index], world);
 }
 
-void ObjectManager::SetTilesWorld(const DirectX::XMMATRIX* arr)
+void ObjectManager::SetTilesWorld(const XMMATRIX* arr)
 {
-	for (int i = 0; i < m_nTiles; i++)
+	for (UINT i = 0; i < m_objTiles->world.size(); i++)
 	{
 		SetTileWorld(i, arr[i]);
 	}
 }
 
-void ObjectManager::SetTileWorld(int index, const DirectX::XMMATRIX &world)
+void ObjectManager::SetTileWorld(int index, const XMMATRIX &world)
 {
 	//Passing world directly into StoreFloat causes random access violation
-	DirectX::XMMATRIX w = world;
-	DirectX::XMStoreFloat4x4(&m_objTiles[index].world, w);
+	XMMATRIX w = world;
+	XMStoreFloat4x4(&m_objTiles->world[index], w);
 }
 
 void ObjectManager::Update()
@@ -303,16 +334,16 @@ void ObjectManager::Update()
 
 void ObjectManager::Render(ID3D11DeviceContext* deviceContext)
 {
-	RenderInstances(deviceContext, m_objPlayer, 1);
-	RenderInstances(deviceContext, m_objEnemies, m_nEnemies);
-	RenderInstances(deviceContext, m_objObstacles, m_nObstacles);
-	RenderInstances(deviceContext, m_objTiles, m_nTiles);
+	RenderInstances(deviceContext, m_objPlayer);
+	RenderInstances(deviceContext, m_objEnemies);
+	RenderInstances(deviceContext, m_objObstacles);
+	RenderInstances(deviceContext, m_objTiles);
 }
 
-void ObjectManager::setViewProjection(const DirectX::XMMATRIX &view, const DirectX::XMMATRIX &projection)
+void ObjectManager::setViewProjection(const XMMATRIX &view, const XMMATRIX &projection)
 {
-	DirectX::XMStoreFloat4x4(&m_view, view);
-	DirectX::XMStoreFloat4x4(&m_projection, projection);
+	XMStoreFloat4x4(&m_view, view);
+	XMStoreFloat4x4(&m_projection, projection);
 }
 
 void ObjectManager::ReleaseCOM()
@@ -320,19 +351,19 @@ void ObjectManager::ReleaseCOM()
 	m_objPlayer->Delete();
 	delete m_objPlayer;
 
-	for (int i = 0; i < m_nEnemies; i++)
+	for (UINT i = 0; i < m_objEnemies->world.size(); i++)
 	{
 		m_objEnemies[i].Delete();
 	}
 	delete[] m_objEnemies;
 
-	for (int i = 0; i < m_nObstacles; i++)
+	for (UINT i = 0; i < m_objObstacles->world.size(); i++)
 	{
 		m_objObstacles[i].Delete();
 	}
 	delete[] m_objObstacles;
 
-	for (int i = 0; i < m_nTiles; i++)
+	for (UINT i = 0; i < m_objTiles->world.size(); i++)
 	{
 		m_objTiles[i].Delete();
 	}
