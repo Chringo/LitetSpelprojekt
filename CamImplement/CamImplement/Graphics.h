e408345d00d29a54a60077e26abdc2cb940c51c7
Zacharias Hortén <zachetank@hotmail.com>
2015-06-05 15:05:59 +0200

    Structure improved
    Code moved from objectManager to Graphics and removed objectManager

--- CamImplement/CamImplement/Graphics.h
+++ CamImplement/CamImplement/Graphics.h
@@ -4,7 +4,7 @@
 #include <d3d11.h>
 #include <d3dcompiler.h>
 #include <DirectXMath.h>
-#include "ObjectManager.h"
+//#include "ObjectManager.h"
 #include "Camera.h"
 #include "GameDummy.h"
 #include "DirectionalLight.h"
@@ -12,6 +12,8 @@
 #include "GUI.h"
 #include <Windows.h>
 #include "ShadowMap.h"
+#include <vector>
+#include "Loader.h"
 
 #pragma comment (lib, "d3d11.lib")
 #pragma comment (lib, "d3dcompiler.lib")
@@ -20,6 +22,59 @@
 
 #define MAX_NUMBER_OF_LIGHTS 10
 #define KEYDOWN(vkey)	(GetAsyncKeyState(vkey) & 0x1)
+
+enum AnimationState
+{
+	Attack,
+	WalkStart,
+	WalkLoop,
+	WalkEnd,
+	AnBlock,
+	AnDodge
+};
+
+#define HUE_DEFAULT	DirectX::XMFLOAT4(1.f, 1.f, 1.f, 1.f)
+#define HUE_HIT		DirectX::XMFLOAT4(2.f, 0.5f, 0.5f, 1.f)
+
+struct InputType
+{
+	InputType() {}
+	InputType(float x, float y, float z,
+		float u, float v,
+		float nx, float ny, float nz) : pos(x, y, z), tex(u, v), nor(nx, ny, nz) {}
+
+	DirectX::XMFLOAT3 pos;
+	DirectX::XMFLOAT2 tex;
+	DirectX::XMFLOAT3 nor;
+};
+
+struct ObjectInstance
+{
+	ID3D11Buffer*		vertexBuffer;
+	ID3D11Buffer*		indexBuffer;
+
+	std::vector<DirectX::XMFLOAT4X4> world;
+	std::vector<bool>	hit;
+
+	InputType*			input;
+	UINT*				indices;
+
+	int					nVertices;
+	int					nIndices;
+	int					nNormals;
+	int					textureIndex;
+	int					frameCount;
+
+	std::vector<float> fx, fy, fz;
+
+	void Delete()
+	{
+		if (vertexBuffer) { vertexBuffer->Release(); }
+		if (indexBuffer) { indexBuffer->Release(); }
+		delete[] input;
+		delete[] indices;
+	}
+};
 
 class Graphics
 {
@@ -36,7 +91,6 @@ private:
 		Light light[MAX_NUMBER_OF_LIGHTS];
 	} cbPointLight;
 
-	ObjectManager*			objManager;
 	GameDummy*				game;
 	Camera*					camera;
 	DirectionalLight*		dirLight;
@@ -61,6 +115,8 @@ private:
 	ShadowMap*				shadowMap;
 	DirectX::XMMATRIX		shadowViewProjection;
 
+	
+
 private:
 	HRESULT CreateDirect3DContext(HWND &wndHandle);
 	void CreateViewport(int width, int height);
@@ -69,6 +125,58 @@ private:
 	void CreateBuffers();
 	void CreateCamera();
 	bool gamePaused;
+
+	/*********************************Object manager**********************************/
+
+private:
+	struct constBufferPerObject
+	{
+		DirectX::XMFLOAT4X4 WVP;
+		DirectX::XMFLOAT4X4 World;
+		DirectX::XMFLOAT4X4 ShadowWVP;
+		DirectX::XMFLOAT4	Hue;
+	}					cbPerObject;
+
+	DirectX::XMFLOAT4X4 m_view;
+	DirectX::XMFLOAT4X4 m_projection;
+	DirectX::XMFLOAT4X4 m_shadowViewProjection;
+
+	Loader*				m_loader;
+
+	ObjectInstance*		m_objPlayer;
+	ObjectInstance*		m_objEnemies;
+	ObjectInstance*		m_objObstacles;
+	ObjectInstance*		m_objTiles;
+
+	// GUI
+	ObjectInstance*		m_objMenu;
+	ObjectInstance*		m_objArrow;
+	int					m_objArrowStateSize;
+	int					currentState;
+	DirectX::XMFLOAT2*	m_objArrowPosState;
+	bool renderMenu;
+
+	ObjectInstance*		m_objWon;
+	bool renderWon;
+
+	ObjectInstance*		m_objLost;
+	bool renderLost;
+	//
+
+	ID3D11Buffer*		cbPerObjectBuffer;
+	ID3D11SamplerState* samplerState;
+	ID3D11SamplerState* pointSampler;
+
+private:
+	void InitInstances(Object obj, ObjectInstance *&object);
+	void CreateBuffers(ID3D11Device* device);
+
+	bool LoadTextures(ID3D11Device* device);
+	void RenderInstances(ID3D11DeviceContext* deviceContext, ObjectInstance* obj);
+	void RenderInstanceGeometry(ID3D11DeviceContext* deviceContext, ObjectInstance *object, const DirectX::XMMATRIX &viewProjection);
+	void CreateSamplers(ID3D11Device* device);
+
+	/***************************************************************************************/
 
 public:
 	Graphics();
@@ -83,6 +191,32 @@ public:
 
 	void SwapFBBuffer();
 	void ReleaseCOM();
+
+	/*********************************Object manager**********************************/
+
+	void objInitialize(ID3D11Device* device, int nEnemies, int nObstacles, int nTiles);
+
+	void SetPlayerHit(bool hit);
+	void SetEnemyHit(int index, bool hit);
+
+	void SetPlayerWorld(const DirectX::XMMATRIX &world);
+	void SetEnemiesWorld(const DirectX::XMMATRIX* arr);
+	void SetObstaclesWorld(const DirectX::XMMATRIX* arr);
+	void SetTilesWorld(const DirectX::XMMATRIX* arr);
+	void SetGUIWorld(const DirectX::XMMATRIX &world);
+
+	// GUI
+	void IncreaseMenuState();
+	void DecreaseMenuState();
+	//
+
+	void SetAnimationState(int index, AnimationState animState);
+
+	void RenderGeometry(ID3D11DeviceContext* deviceContext, const DirectX::XMMATRIX &viewProjection);
+
+	void setViewProjection(const DirectX::XMMATRIX &view, const DirectX::XMMATRIX &projection);
 };
+
+
 
 #endif
