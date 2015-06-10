be1ef92c6ccf631ff052b14f6e558632996747d3
cope13 <corre987@hotmail.com>
2015-06-10 14:41:47 +0200

    Took away the different colours on the enemies
    didn't work with the texture

--- CamImplement/CamImplement/GameDummy.h
+++ CamImplement/CamImplement/GameDummy.h
@@ -34,9 +34,9 @@ private:
 
 	//---------- Enemy types -----
 	const Ent::Enemy regular = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_MEDIUM, 8.f, 100.0f, 0.9f);
-	const Ent::Enemy runner = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_SMALL, 14.f, 80.0f, 0.6f, GREEN);
-	const Ent::Enemy elite = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_MEDIUM, 8.5f, 140.0f, 1.0f, YELLOW);
-	const Ent::Enemy giant = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_LARGE, 7.f, 200.0f, 1.3f, BLUE);
+	const Ent::Enemy runner = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_SMALL, 14.f, 80.0f, 0.6f);
+	const Ent::Enemy elite = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_MEDIUM, 8.5f, 140.0f, 1.0f);
+	const Ent::Enemy giant = Ent::Enemy(DirectX::XMFLOAT3(0, 0, 0), SCALE_LARGE, 7.f, 200.0f, 1.3f);
 	//----------------------------
 
 	unsigned int obsArrSize;
